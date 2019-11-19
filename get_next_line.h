7776ce18cb6646b3e2b0a610227d4a259805a0e3
Cleo REBERT <crebert@student.42.fr>
2019-11-19 16:53:56 +0100

    Semi functionnal, lots of segfaults

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,17 @@
 /*   By: crebert <crebert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 13:56:45 by crebert           #+#    #+#             */
-/*   Updated: 2019/11/19 15:34:31 by crebert          ###   ########.fr       */
+/*   Updated: 2019/11/19 16:40:49 by crebert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 32
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
 typedef struct		s_list
 {
@@ -22,8 +26,10 @@ typedef struct		s_list
 }					t_list;
 
 char				*get_fd_buffer(t_list **start, int fd);
-char				*ft_strndup(char *str, size_t size);
+char				*ft_strndup(char *str, size_t size, int	should_free);
 void				ft_lstclear(t_list **start, int fd);
 size_t				ft_strchr(const char *s);
+char				*ft_strjoin(char const *s1, char const *s2);
+void				ft_lstpushfront(t_list **start, int fd, char *src);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: crebert <crebert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 13:49:11 by crebert           #+#    #+#             */
-/*   Updated: 2019/11/19 15:40:43 by crebert          ###   ########.fr       */
+/*   Updated: 2019/11/19 16:51:56 by crebert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "get_next_line.h"
-
-int		ft_readline(char **str, int fd)
-{
-	char				buffer[BUFFER_SIZE + 1];
-	char				*tmp;
-	int					ret;
-	int					index;
-	static t_list		**maillon = NULL;
-
-	index = -1;
-	ret = 1;
-	tmp = get_fd_buffer(maillon, fd);
-	while (tmp && tmp[++index])
-		if (tmp[index] == '\n')
-			if (!tmp[index + 1])
-				return (ft_strndup(tmp, index));
-	while (ret > 0)
-	{
-		ret = read(fd, buffer, BUFFER_SIZE);
-		if (ret == -1)
-			ft_lstclear(maillon, fd);
-		if (ret == -1)
-			return (-1);
-		buffer[ret] = '\0';
-		tmp = ft_strjoin(tmp, buffer);
-	}
-	str = &tmp;
-	if (!ret)
-		return (0);
-	return (1);
-}
 
 void	ft_lstclear(t_list **start, int fd)
 {
@@ -67,6 +36,8 @@ void	ft_lstclear(t_list **start, int fd)
 		}
 		elem = elem->next;
 	}
+	if (!start || !*start)
+		free(start);
 }
 
 void	ft_lstpushfront(t_list **start, int fd, char *src)
@@ -75,7 +46,7 @@ void	ft_lstpushfront(t_list **start, int fd, char *src)
 
 	if (!(elem = malloc(sizeof(t_list))))
 		return ;
-	elem->buffer = src;
+	elem->buffer = ft_strndup(src, 0, 0);
 	elem->fd = fd;
 	if (start && *start)
 		elem->next = *start;
@@ -88,6 +59,8 @@ size_t	ft_strchr(const char *s)
 {
 	size_t	index;
 
+	if (!s)
+		return (0);
 	index = 0;
 	while (s[index] && s[index] != '\n')
 		index++;
@@ -106,20 +79,22 @@ char	*get_fd_buffer(t_list **start, int fd)
 	char	*res;
 	char	*tmp;
 
+	if (!start)
+		return (NULL);
 	elem = *start;
 	res = NULL;
 	while (elem)
 	{
 		if (elem->fd == fd)
 		{
+			printf("\nRECHERCHE\n");
 			index = ft_strchr(elem->buffer);
-			res = ft_strndup(elem->buffer, index);
+			res = ft_strndup(elem->buffer, index, 0);
 			if (!elem->buffer[index])
 				ft_lstclear(start, fd);
 			else
 			{
-				tmp = ft_strndup(&elem->buffer[index], 0);
-				free(elem->buffer);
+				tmp = ft_strndup(&elem->buffer[index], 0, 1);
 				elem->buffer = tmp;
 			}
 		}
@@ -130,7 +105,7 @@ char	*get_fd_buffer(t_list **start, int fd)
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	index;
+	int		index;
 	size_t	len1;
 	size_t	len2;
 	char	*str;
@@ -139,19 +114,45 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len1 = 0;
 	len2 = 0;
 	if (!s1)
-		return (ft_strndup(s2, 0));
-	if (!s2)
-		return (ft_strndup(s1, 0));
+		return (ft_strndup((char *)s2, 0, 0));
 	while (s1[len1])
 		len1++;
 	while (s2[len2])
 		len2++;
 	if (!(str = malloc(sizeof(char) * (1 + len1 + len2))))
 		return (NULL);
-	while (*s1)
-		str[index++] = *s1++;
+	while (s1[++index])
+		str[index] = s1[index];
 	while (*s2)
 		str[index++] = *s2++;
 	str[index] = 0;
+	if (s1)
+		free((void *)s1);
 	return (str);
+}
+
+char				*ft_strndup(char *str, size_t size, int	should_free)
+{
+	char	*ptr;
+	size_t	index;
+	size_t	len;
+
+	index = 0;
+	len = 0;
+	if (size)
+		len = size;
+	else
+		while (str[len])
+			len++;
+	if (!(ptr = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (index < len)
+	{
+		ptr[index] = str[index];
+		index++;
+	}
+	ptr[index] = 0;
+	if (should_free)
+		free(str);
+	return (ptr);
 }
