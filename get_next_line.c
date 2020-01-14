8ba89508a8f2369eb790e20f6b413dcb806092f5
Cleo REBERT <crebert@student.42.fr>
2020-01-14 16:51:53 +0100

    Fini ? Les tesst plantent mais pas moi ?? weird

--- get_next_line.c
+++ get_next_line.c
@@ -6,150 +6,97 @@
 /*   By: crebert <crebert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 20:49:34 by crebert           #+#    #+#             */
-/*   Updated: 2019/11/27 15:25:44 by crebert          ###   ########.fr       */
+/*   Updated: 2020/01/14 16:48:13 by crebert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-size_t	ft_strlen(char const *str)
+static void	delete_cache(char **cache)
 {
-	size_t	len;
-
-	if (!str)
-		return (0);
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
+	if (*cache)
+	{
+		free(*cache);
+		*cache = NULL;
+	}
 }
 
-char	*ft_strndup(char const *str, int len)
+static void	free_and_replace(char **cache, int fd, char *str)
 {
-	int		str_len;
-	int		index;
-	char	*cpy;
-
-	index = 0;
-	str_len = ft_strlen(str);
-	if (len)
-		str_len = len < str_len ? len : str_len;
-	if (!(cpy = malloc(sizeof(char) * (1 + str_len))))
-		return (NULL);
-	while (*str && (index < str_len || len == 0))
-		cpy[index++] = *str++;
-	cpy[index] = 0;
-	return (cpy);
-}
-
-int		ft_strchr(const char *str, char c)
-{
-	size_t	index;
-
-	if (!str)
-		return (-1);
-	index = 0;
-	while (str[index])
-		if (str[index++] == c)
-			return (index - 1);
-	return (-1);
-}
-
-char	*get_cache(int fd, char **cache)
-{
-	char	*line;
-	char	*tmp;
-	int		index;
-
-	tmp = NULL;
-	line = NULL;
 	if (cache[fd])
 	{
-		index = ft_strchr(cache[fd], '\n');
-		if (index >= 0)
-		{
-			line = ft_strndup(cache[fd], index + 1);
-			tmp = ft_strndup(&cache[fd][index + 1], 0);
-			free(cache[fd]);
-			if (*tmp)
-				cache[fd] = tmp;
-			return (line);
-		}
-		line = ft_strndup(cache[fd], 0);
 		free(cache[fd]);
+		cache[fd] = str;
 	}
-	return (line);
 }
 
-char	*ft_strnjoin(char *s1, char const *s2, int len)
-{
-	size_t	index;
-	int		lens2;
-	char	*str;
+/*
+** cache is a pointer to cache[fd] because otherwise I get weird bugs??
+*/
 
-	lens2 = 0;
-	index = 0;
-	if (!s1)
-		return (ft_strndup(s2, len));
-	if (!(str = malloc(sizeof(char) * (1 + ft_strlen(s1) + ft_strlen(s2)))))
-		return (NULL);
-	while (s1[index])
+static int	get_cache(char **cache, char **line)
+{
+	char	*tmp;
+	size_t	len;
+
+	len = 0;
+	while ((*cache)[len] != 0 && (*cache)[len] != '\n')
+		len++;
+	if ((*cache)[len] == '\n')
 	{
-		str[index] = s1[index];
-		index++;
+		*line = ft_strndup(*cache, len);
+		tmp = ft_strdup(&((*cache)[len + 1]));
+		if (!tmp || *line == NULL)
+			return (GNL_ERROR);
+		free(*cache);
+		*cache = tmp;
+		if ((*cache)[0] == 0)
+			delete_cache(cache);
 	}
-	while (*s2 && (lens2++ < len - 1 || len == 0))
-		str[index++] = *s2++;
-	str[index] = 0;
-	free(s1);
-	return (str);
+	else
+	{
+		if (!(*line = ft_strdup(*cache)))
+			return (GNL_ERROR);
+		delete_cache(cache);
+	}
+	return (GNL_SUCCESS);
 }
 
-int		get_next_line(int fd, char **line)
+static int	get_next_line2(char **cache, int fd, char **line, int ret)
 {
-	char			buffer[BUFFER_SIZE + 1];
-	char			*tmp;
-	static	char	*cache[OPEN_MAX + 1];
-	int				ret;
-	int				index;
+	if (ret == -1)
+		return (GNL_ERROR);
+	if (!cache[fd] && !ret)
+		return (GNL_EOF);
+	return (get_cache(&cache[fd], line));
+}
 
-	ret = 1;
-	tmp = get_cache(fd, cache);
-	index = ft_strchr(tmp, '\n');
-	if (0 <= index)
-		*line = tmp;
-	if (0 <= index)
-		return (1);
-	while (ret > 0 && index < 0)
+int			get_next_line(int fd, char **line)
+{
+	static char	*cache[OPEN_MAX + 1];
+	char		buffer[BUFFER_SIZE + 1];
+	char		*tmp;
+	int			ret;
+
+	if (fd < 0 || !line)
+		return (GNL_ERROR);
+	while ((ret = read(fd, buffer, BUFFER_SIZE)))
 	{
-		ret = read(fd, buffer, BUFFER_SIZE);
 		buffer[ret] = 0;
-		if (ret == -1)
-			return (-1);
-		index = ft_strchr(buffer, '\n');
-		tmp = ft_strnjoin(tmp, buffer, index + 1);
-		if (index >= 0)
-			cache[fd] = ft_strndup(&buffer[index + 1], 0);
+		if (!cache[fd])
+		{
+			if (!(cache[fd] = ft_strdup(buffer)))
+				return (GNL_ERROR);
+		}
+		else
+		{
+			if (!(tmp = ft_strjoin(cache[fd], buffer)))
+				return (GNL_ERROR);
+			free_and_replace(cache, fd, tmp);
+		}
+		if (ft_strchr(cache[fd], '\n'))
+			break ;
 	}
-	if (tmp)
-		*line = tmp;
-	if (tmp)
-		return (1);
-	return (ret);
-}
-
-int		main(int ac, char **av)
-{
-	char	*line = NULL;
-	int		ret = 1;
-	int		index = -1;
-	int		fd = open("get_next_line.h", O_RDONLY);
-	(void)ac;
-	(void)av;
-	while (++index < 20)
-	{
-		ret = get_next_line(fd, &line);
-		printf("%d: %s\n", ret, line);
-	}
+	return (get_next_line2(cache, fd, line, ret));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: crebert <crebert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/27 14:16:53 by crebert           #+#    #+#             */
-/*   Updated: 2019/11/27 15:08:15 by crebert          ###   ########.fr       */
+/*   Updated: 2020/01/14 16:48:52 by crebert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,24 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 4096
+#  define BUFFER_SIZE 9
 # endif
 
+# include <stddef.h>
 # include <sys/uio.h>
 # include <unistd.h>
 # include <limits.h>
 # include <sys/types.h>
 # include <stdlib.h>
 # include <fcntl.h>
+
+size_t	ft_strlen(char const *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strndup(const char *s1, size_t len);
+char	*ft_strdup(const char *s1);
+# define GNL_SUCCESS 1
+# define GNL_EOF 0
+# define GNL_ERROR -1
 
 #endif
